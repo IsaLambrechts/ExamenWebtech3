f9fe3721bdabe5d67134a17d22a1a138d4d00f04
IsaLambrechts <isa.lambrechts@hotmail.be>
2017-09-04 14:13:52 +0200

    registratieklasse

---
+++ Bestelsysteem/src/edu/ap/bestelsysteem/Registratie.java
@@ -0,0 +1,11 @@
+package edu.ap.bestelsysteem;
+
+public class Registratie {
+	
+	private String naam;
+	private String adres;
+	private String datum;
+	private String productnaam;
+	private int hoeveelheid;
+
+}
