b3f7a572066044e88c20bc875c50d45849f30d99
IsaLambrechts <isa.lambrechts@hotmail.be>
2017-09-04 16:22:33 +0200

    getAllProducts

--- ProductenBeheer/src/edu/ap/productbeheer/ProductsResource.java
+++ ProductenBeheer/src/edu/ap/productbeheer/ProductsResource.java
@@ -1,5 +1,14 @@
 package edu.ap.productbeheer;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.restlet.resource.Get;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
@@ -8,7 +17,20 @@ public class ProductsResource extends ServerResource {
 	
 	@Get
 	public String getAllProducts(){
-		return "test";
+		
+		JSONParser parser = new JSONParser();
+
+        Object obj;
+
+        try {
+          obj = parser.parse(new FileReader("C:\\Users\\CaruCath\\Documents\\GitHub\\ExamenWebtech3\\ProductenBeheer\\product.json"));
+          JSONObject jsonObject = (JSONObject) obj;
+          return obj.toString();
+
+        } catch (Exception e) {
+          return e.getMessage();
+        }
+		
 	}
 	
 	@Post
